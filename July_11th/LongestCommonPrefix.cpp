da16eaf63f2fd7401a73a5fabaf565db4446fae2
Rahul Kumar <rahulkumar.workspace@gmail.com>
2026-07-11 23:57:54 +0530

    Longest Common Prefix.cpp

---
+++ July_11th/LongestCommonPrefix.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution{	
+	public:
+		string longestCommonPrefix(vector<string>& str){
+            if (str.empty()) return "";
+
+			sort(str.begin(), str.end());
+
+            string first = str[0];
+            string last = str[str.size()-1];
+
+            int i = 0, j = 0;
+            string ans = "";
+
+            while(i < first.size() && j < last.size()){
+                if(first[i] != last[j]) break;
+
+                ans += first[i];
+                i++;
+                j++;
+            }
+            return ans;
+		}
+};
+
+int main() {
+    Solution solution;
+    vector<string> input = {"flower", "flow", "flight"};
+    string result = solution.longestCommonPrefix(input);
+    cout << "Longest Common Prefix: " << result << endl; // Output: "fl"
+    return 0;
+}
